1cffb00b4a00f8f38eebcdc17b43135be73d3ece
Egor Kuznetsov <ek2kkk@yandex.ru>
2024-02-13 22:35:26 +0300

    first commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set smarttab")
+vim.cmd("set smartindent")
+vim.g.mapleader = " "
+
+vim.cmd("set clipboard+=unnamedplus")
